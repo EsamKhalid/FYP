002051b3903f5ff8432eaece52a12c9503ae4648
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2025-08-25 21:12:59 +0100

    Added match retrieval method, started participant retrieval

--- API_Access.py
+++ API_Access.py
@@ -1,3 +1,5 @@
+import json
+
 from creds import API_Key
 import requests
 
@@ -6,11 +8,23 @@ last_scraped_player = r"C:/Api_Data/Last_Scraped.txt"
 
 headers = {"X-Riot-Token": API_Key}
 
+#seed account = rank 1 player euw
+seed_account ="aSstdTtqhoNflYWFsjsvT1Zg_u0-mJltk_qJ3v80YXazwUlsakbuNzZ8Dsb62jARsH2fbn6rM2q6Ug"
+
+#challenger_list = requests.get("https://euw1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5", headers=headers).json()
 
 
-challenger_list = requests.get("https://br1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5", headers=headers).json()
+def get_player_matches(puuid, max_recency = 7):
+    match_list = requests.get("https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/"+seed_account+"/ids?type=ranked&start=0&count=20", headers=headers).json()
+    return match_list
 
 
-for entry in challenger_list["entries"]:
-    print(entry["puuid"])
+def get_match_participants(matchList):
+    for match_id in matchList:
+        match_data = requests.get("https://europe.api.riotgames.com/lol/match/v5/matches/" + match_id,headers=headers).json()
 
+        print(json.dumps(match_data, indent=4))
+        break
+
+
+get_match_participants(get_player_matches(seed_account))
