6897b0d50a7c5323bdaa30278dbf13d8a27ff590
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2025-08-26 03:11:48 +0100

    Started cross-file work

--- API_Access.py
+++ API_Access.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 from creds import API_Key
 import requests
@@ -11,38 +12,46 @@ match_dir = r"C:/Api_Data/match_data/EUW/"
 HEADERS = {"X-Riot-Token": API_Key}
 
 #seed account = rank 1 player euw
-seed_account ="aSstdTtqhoNflYWFsjsvT1Zg_u0-mJltk_qJ3v80YXazwUlsakbuNzZ8Dsb62jARsH2fbn6rM2q6Ug"
+
 
 #challenger_list = requests.get("https://euw1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5", headers=headers).json()
 
+class ApiAccess:
 
-def get_player_matches(puuid, max_recency = 7):
-    match_list = requests.get("https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/"+seed_account+"/ids?type=ranked&start=0&count=100", headers=HEADERS).json()
-    return match_list
+    def __init__(self, seed):
+        self.seed = seed
 
+    def get_player_matches(self, max_recency=7) -> list :
+        match_list = requests.get(
+            "https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/" + self.seed + "/ids?type=ranked&start=0&count=100",
+            headers=HEADERS).json()
+        return match_list
 
-def get_match_participants(matchList):
-    for match_id in matchList:
-        match_data = requests.get("https://europe.api.riotgames.com/lol/match/v5/matches/" + match_id,headers=HEADERS).json()
-        game_version = match_data["info"]
-        print(json.dumps(match_data, indent=4))
-        match_date = datetime.fromtimestamp(match_data["info"]["gameCreation"] / 1000)
-        match_age = datetime.now() - match_date
-        print(match_age < timedelta(days=7))
+    @staticmethod
+    def get_match_participants(match_list : list):
+        for match_id in match_list:
+            match_data = requests.get("https://europe.api.riotgames.com/lol/match/v5/matches/" + match_id,
+                                      headers=HEADERS).json()
+            game_version = match_data["info"]
+            print(json.dumps(match_data, indent=4))
+            match_date = datetime.fromtimestamp(match_data["info"]["gameCreation"] / 1000)
+            match_age = datetime.now() - match_date
+            print(match_age < timedelta(days=7))
 
-        print(datetime.now() - match_date)
-        time.sleep(1.5)
-        break
+            print(datetime.now() - match_date)
+            time.sleep(1.5)
+            break
 
+    def store_match(patch_version):
+        filePath = match_dir + "1.2.3"
+        if os.path.exists(filePath):
+            print("exists")
+        else:
+            os.mkdir(filePath)
+            print("created")
 
-def store_match(patch_version):
-    filePath = match_dir + "1.2.3"
-    if os.path.exists(filePath):
-        print("exists")
-    else:
-        os.mkdir(filePath)
-        print("created")
 
 #store_match()
 
-get_match_participants(get_player_matches(seed_account))
+
+#get_match_participants(get_player_matches(seed_account))
--- DB_Connect.py
+++ DB_Connect.py
@@ -3,4 +3,12 @@ import psycopg2
 import json
 import os
 
+import API_Access
+
 match_folder = r"C:\Api_Data\match_data"
+
+seed_account ="aSstdTtqhoNflYWFsjsvT1Zg_u0-mJltk_qJ3v80YXazwUlsakbuNzZ8Dsb62jARsH2fbn6rM2q6Ug"
+
+api = API_Access.ApiAccess(seed_account)
+
+print(api.get_player_matches())
