b5041950a06d9074eb91acf367052d602f65d238
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2026-02-25 08:39:46 +0000

    Successful communication between unity and riot games API

--- Assets/Scripts/Cluster/ClusterScript.cs
+++ Assets/Scripts/Cluster/ClusterScript.cs
@@ -8,26 +8,27 @@ using UnityEngine.UI;
 public class ClusterScript : MonoBehaviour
 {
     public GameObject playerPrefab;
-    public TMP_InputField inputField;
+    public TMP_InputField nameField;
+    public TMP_InputField tagField;
     public void OnSubmitButton()
     {
-        string riotID = inputField.text;
-        StartCoroutine(GetPlayerData(riotID));
+        string name = nameField.text;
+        string tag = tagField.text;
+        StartCoroutine(GetPlayerData(name, tag));
     }
 
-    IEnumerator GetPlayerData(string riotID)
+    IEnumerator GetPlayerData(string name, string tag)
     {
-        string url = "http://127.0.0.1:8000/player/" + riotID;
+        string url = "http://127.0.0.1:8000/clusterManager/" + name + "/" + tag;
         UnityWebRequest request = UnityWebRequest.Get(url);
 
         yield return request.SendWebRequest();
 
         if (request.result == UnityWebRequest.Result.Success)
         {
-            PlayerResponse data =
-                JsonUtility.FromJson<PlayerResponse>(request.downloadHandler.text);
+            PlayerResponse data = JsonUtility.FromJson<PlayerResponse>(request.downloadHandler.text);
 
-            SpawnPlayer(data);
+            handleResponse(data);
         }
         else
         {
@@ -35,10 +36,11 @@ public class ClusterScript : MonoBehaviour
         }
     }
 
-    void SpawnPlayer(PlayerResponse data)
+    void handleResponse(PlayerResponse data)
     {
         Vector3 position = new Vector3(data.x, data.y, data.z);
         Instantiate(playerPrefab, position, Quaternion.identity);
+        Debug.Log(data.puuid);
     }
 }
 
@@ -49,4 +51,5 @@ public class PlayerResponse
     public float y;
     public float z;
     public int cluster;
+    public string puuid;
 }
--- PythonScripts/main.py
+++ PythonScripts/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
 import random
+import requests
+from creds import API_KEY
+
 
 app = FastAPI()
 
-@app.get("/player/{riot_id}")
-def get_player(riot_id: str):
+def get_puuid(name : str, tag : str) -> str:
+    response = requests.get(f"https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tag}?api_key={API_KEY}")
+    return response.json()["puuid"]
+
+@app.get("/clusterManager/{name}/{tag}")
+
+def get_player(name : str, tag : str):
     return {
         "x": random.uniform(-5, 5),
         "y": random.uniform(-5, 5),
         "z": random.uniform(-5, 5),
-        "cluster": random.randint(0, 2)
+        "cluster": random.randint(0, 2),
+        "puuid" : get_puuid(name,tag),
     }
