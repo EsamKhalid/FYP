9fe00135f5f241c55a78d93c1ef71959d000504f
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2026-03-26 05:32:20 +0000

    Features extracted and pca performed,

--- PythonScripts/timeline_processor.py
+++ PythonScripts/timeline_processor.py
@@ -1,45 +1,65 @@
 import json
-import pandas
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 json_path = '../data/EUW1_7612763297_timeline.json'
 
+rootdir = "C:/Api_Data/combined/timeline_data/EUW"
 
 
-with open(json_path, 'r') as f:
-    data = json.load(f)
+def process_timelines():
+    with open(json_path, 'r') as f:
+        data = json.load(f)
 
-player_features = []
+    player_features = []
 
-puuid_list = data["info"]["participants"]
+    puuid_list = data["info"]["participants"]
 
-frames = data['info']['frames']
-frame1 = data["info"]["frames"][0]["participantFrames"]
-frame10 = data["info"]["frames"][10]["participantFrames"]
-frame15 = data["info"]["frames"][15]["participantFrames"]
-last_frame = data["info"]["frames"][-1]["participantFrames"]
+    frames = data['info']['frames']
+    frame1 = data["info"]["frames"][0]["participantFrames"]
+    frame10 = data["info"]["frames"][10]["participantFrames"]
+    frame15 = data["info"]["frames"][15]["participantFrames"]
+    last_frame = data["info"]["frames"][-1]["participantFrames"]
 
-match_length = len(frames)
+    match_length = len(frames)
 
-def calculate_movement():
+    for id in range(1, 10):
+        player_features.append({
+            "puuid": puuid_list[id - 1]["puuid"],
+            "gold@10": frame10[str(id)]["totalGold"],
+            "gold@15": frame15[str(id)]["totalGold"],
+            "cs@10": frame10[str(id)]["minionsKilled"] + frame10[str(id)]["jungleMinionsKilled"],
+            "cs@15": frame15[str(id)]["minionsKilled"] + frame15[str(id)]["jungleMinionsKilled"],
+            "gpm": last_frame[str(id)]["totalGold"] / match_length,
+            "cspm": (last_frame[str(id)]["minionsKilled"] + last_frame[str(id)]["jungleMinionsKilled"]) / match_length,
+            "dpm": last_frame[str(id)]["damageStats"]["totalDamageDone"] / match_length,
+            "timeEnemyCC": last_frame[str(id)]["timeEnemySpentControlled"],
+        })
 
-    pass
+    def perform_pca():
+        df = pd.DataFrame(player_features)
 
-for id in range(1,10):
-    player_features.append({
-        "puuid" : puuid_list[id - 1]["puuid"],
-        "gold@10" : frame10[str(id)]["totalGold"],
-        "gold@15" : frame15[str(id)]["totalGold"],
-        "cs@10" : frame10[str(id)]["minionsKilled"] + frame10[str(id)]["jungleMinionsKilled"],
-        "cs@15" : frame15[str(id)]["minionsKilled"] + frame15[str(id)]["jungleMinionsKilled"],
-        "gpm" : last_frame[str(id)]["totalGold"] / match_length,
-        "cspm" : (last_frame[str(id)]["minionsKilled"] + last_frame[str(id)]["jungleMinionsKilled"]) / match_length,
-        "dpm" : last_frame[str(id)]["damageStats"]["totalDamageDone"] / match_length,
-        "timeEnemyCC" : last_frame[str(id)]["timeEnemySpentControlled"],
-    })
+        features = ["gold@10", "gold@15", "cs@10", "cs@15", "gpm", "cspm", "dpm", "timeEnemyCC"]
+        scaled = StandardScaler().fit_transform(df[features])
 
+        pca = PCA(n_components=3)
+        pca_results = pca.fit_transform(scaled)
 
-print(player_features)
+        loadings = pd.DataFrame(
+            pca.components_.T,
+            columns=['PC1', 'PC2', 'PC3'],
+            index=features
+        )
 
-print(json.dumps(frame10, indent=4))
+        print(loadings)
 
+        return [{
+            "puuid": df.iloc[i]["puuid"],
+            "x": float(pca_results[i, 0]),
+            "y": float(pca_results[i, 1]),
+            "z": float(pca_results[i, 2])
+        } for i in range(len(df))]
+
+    perform_pca()
 
