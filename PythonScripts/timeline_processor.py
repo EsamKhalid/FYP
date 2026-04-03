676d46aa5221ab98425a6653bde47bbf6ab12409
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2026-04-03 08:23:39 +0100

    Severely optimised timeline processing code

--- PythonScripts/timeline_processor.py
+++ PythonScripts/timeline_processor.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 import os
 import psycopg2
-from psycopg2.extras import DictCursor
+from psycopg2.extras import DictCursor, execute_values
 
 from PythonScripts.creds import DBPASS
 
@@ -12,6 +12,8 @@ rootdir = "C:/Api_Data/combined/timeline_data/EUW"
 rawParticipants = pd.read_csv("../data/participants.csv")
 
 participants = pd.DataFrame(rawParticipants.set_index(["puuid", "match_id"])[["lane", "champion_name", "kills", "deaths", "assists", "vision_score"]].to_dict())
+
+count = 0
 
 class TimelineProcessor():
 
@@ -29,65 +31,67 @@ class TimelineProcessor():
 
     def insert_player_feature(self, player_feature):
         self.cur.execute(f"INSERT INTO player_features (puuid,match_id,lane,gold_7,gold_15,cs_7,cs_15,xp_7,xp_15,gpm,cspm,xpm,dpm)"
-                         f"VALUES ('{player_feature["puuid"]}','{player_feature["match_id"]}','{player_feature["lane"]}','{player_feature["gold_7"]}','{player_feature["gold_15"]}','{player_feature["cs_7"]}','{player_feature["cs_15"]}','{player_feature["xp_7"]}','{player_feature["xp_15"]}','{player_feature["gpm"]}','{player_feature["cspm"]}','{player_feature["xpm"]}','{player_feature["dpm"]}')")
+                         f"VALUES ('{player_feature["puuid"]}','{player_feature["match_id"]}','{player_feature["lane"]}','{player_feature["gold_7"]}','{player_feature["gold_15"]}','{player_feature["cs_7"]}','{player_feature["cs_15"]}','{player_feature["xp_7"]}','{player_feature["xp_15"]}','{player_feature["gpm"]}','{player_feature["cspm"]}','{player_feature["xpm"]}','{player_feature["dpm"]}')"
+                         f"ON CONFLICT DO NOTHING")
         self.conn.commit()
 
-    def process_timelines(self):
-
-        matches = 0
-
+    def process_timelines(self, count):
+        batch_buffer = []
         for subdir, dir, files in os.walk(rootdir):
             for file in files:
-                parts = file.rsplit('_', 1)
-                match_id = parts[0]
-                filepath = subdir + "/" + file
+                match_id = file.rsplit('_', 1)[0]
+                filepath = os.path.join(subdir, file)
                 with open(filepath, "r") as f:
-                    patch_version = subdir.rsplit("\\", 1)[1]
-
+                    #patch_version = subdir.rsplit("\\", 1)[1]
                     data = json.load(f)
-
                     player_features = []
-
-                    lane = ""
-
                     puuid_list = data["metadata"]["participants"]
-
                     frames = data['info']['frames']
-
                     match_length = len(frames)
 
                     if match_length < 15:
                         continue
 
-                    frame7 = data["info"]["frames"][6]["participantFrames"]
-                    frame15 = data["info"]["frames"][14]["participantFrames"]
-                    last_frame = data["info"]["frames"][-1]["participantFrames"]
+                    frame7 = frames[6]["participantFrames"]
+                    frame15 = frames[14]["participantFrames"]
+                    last_frame = frames[-1]["participantFrames"]
 
                     for id in range(1, 11):
-                        f7 = frame7[str(id)]
-                        f15 = frame15[str(id)]
-                        lf = last_frame[str(id)]
+                        str_id = str(id)
+                        f7 = frame7[str_id]
+                        f15 = frame15[str_id]
+                        lf = last_frame[str_id]
                         puuid = puuid_list[id - 1]
-                        player_features.append({
-                            "puuid": puuid,
-                            "match_id" : match_id,
-                            "lane" : participants.loc[(puuid, match_id), "lane"],
-                            "gold_7": f7["totalGold"],
-                            "gold_15": f15["totalGold"],
-                            "cs_7": f7["minionsKilled"] + f7["jungleMinionsKilled"],
-                            "cs_15": f15["minionsKilled"] + f15["jungleMinionsKilled"],
-                            "xp_7": f7["xp"],
-                            "xp_15": f15["xp"],
-                            "gpm": round(lf["totalGold"] / match_length),
-                            "cspm": round((lf["minionsKilled"] + lf["jungleMinionsKilled"]) / match_length),
-                            "xpm": round(lf["xp"] / match_length),
-                            "dpm": round(lf["damageStats"]["totalDamageDoneToChampions"] / match_length),
-                        })
-                self.insert_player_feature(player_features[0])
-                exit()
+                        lane = participants.loc[(puuid, match_id), "lane"]
+                        batch_buffer.append((
+                            puuid, match_id, lane,
+                            f7["totalGold"], f15["totalGold"],
+                            f7["minionsKilled"] + f7["jungleMinionsKilled"],
+                            f15["minionsKilled"] + f15["jungleMinionsKilled"],
+                            f7["xp"], f15["xp"],
+                            round(lf["totalGold"] / match_length),
+                            round((lf["minionsKilled"] + lf["jungleMinionsKilled"]) / match_length),
+                            round(lf["xp"] / match_length),
+                            round(lf.get("damageStats", {}).get("totalDamageDoneToChampions", 0) / match_length)
+                        ))
 
+                if len(batch_buffer) >= 100:
+                    self.batch_insert(batch_buffer)
+                    count += len(batch_buffer)
+                    print(f"inserted {count} players")
+                    batch_buffer = []
+
+        if batch_buffer:
+            self.batch_insert(batch_buffer)
+
+    def batch_insert(self, data):
+        query = """INSERT INTO player_features (puuid, match_id, lane, gold_7, gold_15, cs_7, cs_15, xp_7, xp_15, gpm, cspm, xpm, dpm) 
+                    VALUES %s 
+                    ON CONFLICT DO NOTHING """
+        execute_values(self.cur, query, data)
+        self.conn.commit()
 timelineProcessor = TimelineProcessor()
 
-timelineProcessor.process_timelines()
+timelineProcessor.process_timelines(count)
 
 
